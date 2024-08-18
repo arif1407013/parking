9d5a71f2adeec32ce6fbbf5be9988c1c29b9fdb9
Arif <arifurrahmankislu@gmail.com>
2024-08-18 15:27:58 +0600

    firebase module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FirebaseAppModule } from '@angular/fire/app';
+import { FirestoreModule } from '@angular/fire/firestore';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, NgbPopoverModule],
+  imports: [RouterOutlet, RouterModule, NgbPopoverModule, FirestoreModule, FirebaseAppModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
