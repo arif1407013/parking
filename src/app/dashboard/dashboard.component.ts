38ffffae82c6497a58e9993abc9a853cfc3bb052
Arif <arifurrahmankislu@gmail.com>
2024-08-18 17:19:12 +0600

    added dashboard filter

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,25 +2,34 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { InternalDataService } from '../DataService/internal-data.service';
 import { ChartsComponent } from '../charts/charts.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, ChartsComponent],
+  imports: [CommonModule, ChartsComponent, FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent implements OnInit {
   protected dashboard_data: any;
 
+  protected dateData: string = '';
+
   ngOnInit(): void {
+    this.dateData = `${new Date().getFullYear()}-${`${new Date().getMonth()+1}`.padStart(2, '0')}-${new Date().getDate()}`;
+    this.getData();
+  }
+
+  getData(){
+    let tempList: any[] = this.internal_data.getDataList()?.filter((vehicle: any) => vehicle?.car_entry_date === this.dateData);
     this.dashboard_data = {
-      total_parked: this.internal_data.getDataList()?.filter((vehicle: any) => vehicle?.status === 'In')?.length,
-      total_empty: this.internal_data.getDataList()?.filter((vehicle: any) => vehicle?.status === 'Out')?.length,
-      total_microbus: this.internal_data.getDataList()?.filter((vehicle: any) => vehicle?.v_type === 'Microbus')?.length,
-      total_car: this.internal_data.getDataList()?.filter((vehicle: any) => vehicle?.v_type === 'Car')?.length,
-      total_truck: this.internal_data.getDataList()?.filter((vehicle: any) => vehicle?.v_type === 'Truck')?.length,
-      total_overtime: this.internal_data.getDataList()?.filter((vehicle: any) => (vehicle?.status === 'In' ? new Date()?.getTime() : vehicle?.status === 'Out' ? new Date(vehicle?.car_exit)?.getTime() : 0) - new Date(vehicle?.car_entry)?.getTime() > 7200000 )?.length,
+      total_parked: tempList?.filter((vehicle: any) => vehicle?.status === 'In')?.length,
+      total_empty: tempList?.filter((vehicle: any) => vehicle?.status === 'Out')?.length,
+      total_microbus: tempList?.filter((vehicle: any) => vehicle?.v_type === 'Microbus')?.length,
+      total_car: tempList?.filter((vehicle: any) => vehicle?.v_type === 'Car')?.length,
+      total_truck: tempList?.filter((vehicle: any) => vehicle?.v_type === 'Truck')?.length,
+      total_overtime: tempList?.filter((vehicle: any) => (vehicle?.status === 'In' ? new Date()?.getTime() : vehicle?.status === 'Out' ? new Date(vehicle?.car_exit)?.getTime() : 0) - new Date(vehicle?.car_entry)?.getTime() > 7200000 )?.length,
     }
   }
 
